4f7d90a2a007b06401142390bc0d4047f4edfe37
Rodrigo da Silva <rdasilva@fispan.com>
2021-10-22 15:27:51 -0300

    Finished Section 5.

---
+++ react/seasons/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+
+    // React says we HAVE to define render!!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
