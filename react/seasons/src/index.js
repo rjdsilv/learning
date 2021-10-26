c2dff70150e6a0096aea1596a5653b8b0ef0dd55
Rodrigo da Silva <rdasilva@fispan.com>
2021-10-26 10:56:23 -0300

    Finished Section 6.

--- react/seasons/src/index.js
+++ react/seasons/src/index.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
 
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
         );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it re-rendered!');
     }
 
     // React says we HAVE to define render!!!
     render() {
+        return <div>{this.renderContent()}</div>;
+    }
+
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>;
         }
-        return <div>Loading...</div>
+        return <Spinner message="Please, accept the location request"/>;
     }
 }
 
