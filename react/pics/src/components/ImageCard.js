ecb598ef43a5b6597590b925310106e5a918e503
Rodrigo da Silva <rdasilva@fispan.com>
2021-11-25 15:12:13 -0300

    Quick fix

--- react/pics/src/components/ImageCard.js
+++ react/pics/src/components/ImageCard.js
@@ -10,17 +10,17 @@ class ImageCard extends React.Component {
         };
     }
 
-    componentDidMount() {
+    componentDidMount = () => {
         this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
     setSpans = () => {
         const height = this.imageRef.current.clientHeight;
-        const spans = Math.ceil(height / 10);
+        const spans = Math.ceil(height / 10) + 1;
         this.setState({spans})
     }
 
-    render() {
+    render = () => {
         const {alt_description, urls} = this.props.image;
         return (
             <div style={{gridRowEnd: `span ${this.state.spans}`}}>
