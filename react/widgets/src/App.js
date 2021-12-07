014d04c3be8d854b26506bbac3f35fd0a9ef3669
Rodrigo da Silva <rdasilva@fispan.com>
2021-12-07 10:41:04 -0300

    Finished DropDown Component.

--- react/widgets/src/App.js
+++ react/widgets/src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
 import Search from './components/Search';
+import DropDown from './components/DropDown';
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers'
+    },
+    {
+        title: 'Who do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <Search/>
+            <DropDown selected={selected}
+                      options={options}
+                      label="Select a Color"
+                      onSelectedChange={setSelected}
+            />
         </div>
     );
 };
---
+++ react/widgets/src/components/DropDown.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+const DropDown = ({selected, options, label, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (!ref.current.contains(event.target)) {
+                setOpen(false);
+            }
+
+        };
+
+        document.body.addEventListener('click', onBodyClick, {capture: true});
+
+        // Runs whenever DropDown is removed from the DOM.
+        return () => {
+            document.body.removeEventListener('click', onBodyClick, {capture: true});
+        };
+    }, []);
+
+    const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
+        return (
+            <div key={option.value}
+                 className="item"
+                 onClick={() => onSelectedChange(option)}
+            >
+                {option.label}
+            </div>
+        );
+    });
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">{label}</label>
+                <div className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                     onClick={() => setOpen(!open)}
+                >
+                    <i className="dropdown icon"/>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DropDown;
