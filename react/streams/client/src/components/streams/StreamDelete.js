9025e7588e266901882c47fc04a0b3d19068d7ed
Rodrigo da Silva <rdasilva@fispan.com>
2022-04-21 10:19:47 -0300

    Finished Section 25

--- react/streams/client/src/components/streams/StreamDelete.js
+++ react/streams/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Modal from '../Modal'
+import browserHistory from '../../browserHistory'
+import { connect } from 'react-redux'
+import { fetchStream, deleteStream } from '../../actions'
 
-const StreamDelete = () => {
+const StreamDelete = ({ match, stream, fetchStream: getStream, deleteStream: removeStream }) => {
+
+  useEffect(() => {
+    getStream(match.params.id)
+  }, [])
+
   return (
-    <div>StreamDelete</div>
+    <Modal header="Delete Stream"
+           content={`Are you sure you want to delete the "${stream ? stream.title : ''}" stream?`}
+           actions={
+             <React.Fragment>
+               <button onClick={() => removeStream(stream.id)}
+                       className="ui negative button"
+               >
+                 Delete
+               </button>
+               <Link to="/" className="ui button">
+                 Cancel
+               </Link>
+             </React.Fragment>
+           }
+           onDismiss={() => browserHistory.push('/')}
+    />
   )
 }
 
-export default StreamDelete
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream },
+)(StreamDelete)
